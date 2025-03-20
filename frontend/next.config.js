63bdf397d6ae92d9c39690b20112246e4b029eee
Nigel Holder <nholder88@gmail.com>
2025-03-19 22:42:01 -0400

    fix(frontend): update Next.js config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,23 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    output: 'standalone',
     async rewrites() {
         return [
             {
                 source: '/api/chores/:path*',
-                destination: process.env.CHORE_SERVICE_URL + '/:path*',
+                destination: (process.env.CHORE_SERVICE_URL || 'http://chore-service:3001') + '/:path*',
             },
             {
                 source: '/api/inventory/:path*',
-                destination: process.env.INVENTORY_SERVICE_URL + '/:path*',
+                destination: (process.env.INVENTORY_SERVICE_URL || 'http://inventory-service:8000') + '/:path*',
             },
             {
                 source: '/api/shopping/:path*',
-                destination: process.env.SHOPPING_SERVICE_URL + '/:path*',
+                destination: (process.env.SHOPPING_SERVICE_URL || 'http://shopping-service:5000') + '/:path*',
             },
             {
                 source: '/api/households/:path*',
-                destination: process.env.HOUSEHOLD_SERVICE_URL + '/:path*',
+                destination: (process.env.HOUSEHOLD_SERVICE_URL || 'http://household-service:3002') + '/:path*',
             },
         ];
     },
