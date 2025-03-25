458d0b64b5bd1cd64bf930160d5d956772466b4f
Nigel Holder <nholder88@gmail.com>
2025-03-25 15:06:36 -0400

    chore(config): update Angular app config and add Next.js PostCSS config

--- frontend/angular/src/app/app.config.ts
+++ frontend/angular/src/app/app.config.ts
@@ -1,11 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideEffects } from '@ngrx/effects';
 
 import { authInterceptor } from './services/auth.interceptor';
+import { userReducer } from './store/user/user.reducer';
+import { UserEffects } from './store/user/user.effects';
+import { householdReducer } from './store/household/household.reducer';
+import { choresReducer } from './store/chores/chores.reducer';
+import { HouseholdEffects } from './store/household/household.effects';
+import { ChoresEffects } from './store/chores/chores.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,5 +22,18 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideAnimations(),
+    provideStore({
+      user: userReducer,
+      household: householdReducer,
+      chores: choresReducer,
+    }),
+    provideEffects([UserEffects, HouseholdEffects, ChoresEffects]),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    }),
   ],
 };
